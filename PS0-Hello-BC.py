24c895aff60b9267b03ca131dd9067caf448420d
Jessie Finocchiaro <22jjf22@gmail.com>
2025-08-26 13:38:22 -0400

    removing student submission

--- PS0-Hello-BC.py
+++
@@ -1,7 +0,0 @@
-Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
->>> 
-======== RESTART: /Users/meidasgupta/Desktop/Data Science Course/ps0.py ========
-Hello, Boston College!!
-22
-BC, I think this is the beginning of a beautiful friendship.
--- ps0.py
+++ ps0.py
@@ -1,4 +1,2 @@
-print('Hello, Boston College!!')
-print(7+15)
-print("BC, I think this is the beginning of a beautiful friendship.")
+print('Hello world')
 
